c15a03329c253b6ff7dc077378d24c6e22637caa
raymondfdavey <raymondfdavey@gmail.com>
2020-04-19 10:59:18 +0100

    initial commit'

---
+++ src/components/Date.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Date extends Component {
+  render() {
+    return (
+      <div>
+        <p>date bit</p>
+      </div>
+    );
+  }
+}
+
+export default Date;
