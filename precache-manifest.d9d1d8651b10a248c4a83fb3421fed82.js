65dfe4b1b73209b259b060b07b4227b69f5b02ec
raymondfdavey <raymondfdavey@gmail.com>
2020-04-19 11:00:02 +0100

    Updates

---
+++ precache-manifest.d9d1d8651b10a248c4a83fb3421fed82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a574610eed4658c2808bea4fbd4eb6",
+    "url": "/golascroll/index.html"
+  },
+  {
+    "revision": "4f6100baed3ca6309c07",
+    "url": "/golascroll/static/css/main.2b344756.chunk.css"
+  },
+  {
+    "revision": "7dd8128f8fc311d8365b",
+    "url": "/golascroll/static/js/2.573722a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/golascroll/static/js/2.573722a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6100baed3ca6309c07",
+    "url": "/golascroll/static/js/main.edaab928.chunk.js"
+  },
+  {
+    "revision": "5b27a8aea84b879dcddf",
+    "url": "/golascroll/static/js/runtime-main.c2d48914.js"
+  },
+  {
+    "revision": "a2120ce1d3a5081ff95ba20d6a0481c5",
+    "url": "/golascroll/static/media/Hearts.a2120ce1.jpg"
+  }
+]);
